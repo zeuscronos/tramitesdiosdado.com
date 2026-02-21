64e66afb9b3d1be998c435080223296993bedc31
Angel Hernandez <angelherriv@gmail.com>
2026-02-21 22:00:41 +0100

    removed unused themes + installed plugin: wp-super-cache

--- wp-config.php
+++ wp-config.php
@@ -39,6 +39,9 @@ if (!function_exists('getenv_docker')) {
 	}
 }
 
+define('WP_CACHE', true);
+define( 'WPCACHEHOME', __DIR__ . '/wp-content/plugins/wp-super-cache/' );
+
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', getenv_docker('WORDPRESS_DB_NAME', 'wordpress') );
